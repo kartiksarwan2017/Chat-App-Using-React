2af5adbd1792fe224bd01cf39edcaed17a2d0adc
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-07-05 10:16:45 +0530

    React Chat App Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat";
+import './App.css';
   
 
 function App() {
